dae27fdd92e31d82be35590d88b643012a1b5258
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2025-01-30 22:44:38 -0500

    Se agrega componente Card

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,5 +1,11 @@
 import { Layout } from '../../Components/Layout';
+import { Card } from '../../Components/Card';
 
 export const Home = () => {
-   return <Layout>Home</Layout>;
+   return (
+      <Layout>
+         Home
+         <Card />
+      </Layout>
+   );
 };
