16e56aca72e3191e51c5afeac379e701a0691b2a
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2025-02-03 22:15:39 -0500

    Se agrega componente Card

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,30 @@
+import { useState, useEffect } from 'react';
+
 import { Layout } from '../../Components/Layout';
 import { Card } from '../../Components/Card';
 
 export const Home = () => {
+   //Estados
+   const [items, setItems] = useState(null);
+
+   //Consumir API
+   useEffect(() => {
+      fetch('https://api.escuelajs.co/api/v1/products')
+         .then((response) => response.json())
+         .then((data) => {
+            setItems(data);
+            console.log(data);
+         });
+   }, []);
+
    return (
       <Layout>
          Home
-         <Card />
+         <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+            {items?.map((item) => (
+               <Card key={item.id} data={item} />
+            ))}
+         </div>
       </Layout>
    );
 };
